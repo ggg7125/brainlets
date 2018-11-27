b1eea94da221e58325e4ccfe2eb8bf78b701a61c
Hello <ggg7125@gmail.com>
2018-11-27 11:33:13 -0500

    stopped the inclusion of the source files

--- notes/reference/codehelp.js
+++
@@ -1,5 +0,0 @@
-//setting array2 = array1 creates a REFERENCE not a copy. to get a copy do array2 = array1.slice()
-
-var fs = require('fs') //file system, can write txt files and stuff
-fs.writeFileSync("hello.txt", "hello my friend")
-console.log(fs.readFileSync("hello.txt").toString())
